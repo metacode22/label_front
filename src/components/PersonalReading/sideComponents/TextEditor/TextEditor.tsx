56e87c3fadf9907b6ecb958822ca04ac57ce7de3
joonhyukchoi <cjh4787@gmail.com>
2022-08-03 03:45:10 +0900

    feat: editor

--- src/components/PersonalReading/sideComponents/TextEditor/TextEditor.tsx
+++ src/components/PersonalReading/sideComponents/TextEditor/TextEditor.tsx
@@ -1,5 +1,5 @@
 import { editorViewOptionsCtx } from '@milkdown/core';
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+import { defaultValueCtx, Editor, rootCtx, JSONRecord } from '@milkdown/core';
 import { listener, listenerCtx } from '@milkdown/plugin-listener';
 import { prism } from '@milkdown/plugin-prism';
 import { tooltip } from '@milkdown/plugin-tooltip';
@@ -14,12 +14,13 @@ import { history } from '@milkdown/plugin-history'
 import io from 'socket.io-client';
 import React from 'react';
 import './TextEditor.css';
+import axios from "axios";
 
 const url:string = 'https://tradingstudy.shop:443';
 let socket:any;
 
 let timerId:NodeJS.Timeout
-export class WrapperTextEditor extends React.Component<{markdownValue: string, commitIdx: number, userIdx: string, pdfIdx: string}, { fp: string, flag: boolean }> {
+export class WrapperTextEditor extends React.Component<{markdownValue: JSONRecord, commitIdx: number, userIdx: string, pdfIdx: string}, { fp: any, flag: boolean }> {
     constructor(props: any) {
         super(props);
         socket = io(url, {
@@ -30,9 +31,8 @@ export class WrapperTextEditor extends React.Component<{markdownValue: string, c
             },
         });
         
-        this.state = {fp: 'hello', flag: false};
+        this.state = {fp: {}, flag: false};
         socket.on('connect',() => {
-            // console.log('connect-------------');
             socket.once('updateEditorOnce', (value: any) => {
                 this.setState({
                     fp: value,
@@ -46,9 +46,7 @@ export class WrapperTextEditor extends React.Component<{markdownValue: string, c
         }
     }
 
-
     render() {
-        // console.log('value in render:', this.state.fp, this.state.flag)
         return (
             <div>
                 {this.state.flag && <TextEditor value={this.state.fp} markdownValue={this.props.markdownValue} commitIdx={this.props.commitIdx} userIdx={this.props.userIdx} pdfIdx={this.props.pdfIdx}/>}
@@ -65,42 +63,124 @@ export class WrapperTextEditor extends React.Component<{markdownValue: string, c
 }
 
 /* solution 2 */
-function updateEditor(userID : string, pdfID : string, value : string) {
+function updateEditor(userID : string, pdfID : string, value : any) {
     timerId = setTimeout(() => {
         socket.emit("updateEditor", {id: userID, pdfId: pdfID, text: value}, clearTimeout(timerId));
     }, 700)
 }
 
-export const TextEditor: FC<{ value: string, markdownValue: string, commitIdx: number, userIdx: string, pdfIdx: string }> = ({ value, markdownValue, commitIdx, userIdx, pdfIdx }) => {
+async function updateOrClearEditor(userID: string, pdfID : string, jsonOutput : any) {
+    if (timerId) {
+        clearTimeout(timerId);
+        updateEditor(userID, pdfID, jsonOutput)
+    } else {
+        updateEditor(userID, pdfID, jsonOutput)
+    }
+}
+
+async function imageHandler(imageSrc : any, userID : string) {
+    const { preSignedUrl, fileName } : any = await getPresignedUrl(userID);
+    const imageFile = await imageSrcToFile(imageSrc, fileName);
+    const imageUrl = `https://label-editor.s3.ap-northeast-2.amazonaws.com/${fileName}`;
+
+    await uploadImageToS3(preSignedUrl.signedUrlPut, imageFile);
+    return imageUrl
+}
+
+async function getPresignedUrl(userID : string) {
+    try {
+        const randomString = Math.random().toString(36).substring(2, 11);
+        const fileName = `${userID}_${randomString}.png`;
+        const response = await axios.post("https://tradingstudy.shop:443/users/sign", {
+            fileName
+        });
+        return {preSignedUrl: response.data, fileName};
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function imageSrcToFile(imageSrc : any, fileName : any) {
+    var arr = imageSrc.split(','), mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, {type:mime});
+}
+
+async function uploadImageToS3(preSignedUrl : any, imageFile : any) {
+    const response = await fetch(preSignedUrl, {
+        method: 'PUT',
+        body: imageFile,
+        headers: {
+        'x-amz-acl':'public-read',
+        'Content-Type': 'image/png',
+        },
+    })
+}
+
+export const TextEditor: FC<{ value: JSONRecord, markdownValue: JSONRecord, commitIdx: number, userIdx: string, pdfIdx: string }> = ({ value, markdownValue, commitIdx, userIdx, pdfIdx }) => {
     // let readonly = false;
     // const editable = () => !readonly;
     const { editor, loading, getInstance } = useEditor((root, renderReact) => {
                 const editor = Editor.make()
                     .config((ctx) => {
                         ctx.set(rootCtx, root);
-                        // ctx.set(editorViewOptionsCtx, { editable })
-                        
-                        if (commitIdx === -1) {
-                            ctx.set(defaultValueCtx, value);    
-                        } else {
+                        // 처음 생성된 editor라면 빈 화면 생성
+                        // 그게 아니라면 JSON형식으로 DB에서 불러들임
+                        if (commitIdx === -1 && value) {
+                            ctx.set(defaultValueCtx, {
+                                type: "json",
+                                value: value,
+                            });    
+                        } else if (commitIdx === -1 && Object.keys(value).length === 0) {
                             // console.log('-----------------', markdownValue);
-                            ctx.set(defaultValueCtx, markdownValue);  
+                            ctx.set(defaultValueCtx, '');  
+                        } else {
+                            ctx.set(defaultValueCtx, {
+                                type: "json",
+                                value: markdownValue,
+                            });
+                            console.log(markdownValue);
                         }
                         
-                        ctx.get(listenerCtx).markdownUpdated((_, value) => {
-                            const userID = userIdx;
-                            const pdfID = pdfIdx;
-                            /* solution 2 */
-                            if (timerId) {
-                                clearTimeout(timerId);
-                                updateEditor(userID, pdfID, value)
-                                console.log('clear')
-                            } else {
-                                updateEditor(userID, pdfID, value)
-                                console.log('update')
+                        ctx.get(listenerCtx).updated(async (ctx, doc, prevDoc) => {
+                            let jsonOutput = doc.toJSON();
+                            console.log('jsonOutput: ',jsonOutput);
+                            let arrOutput = jsonOutput.content;
+                            for (let i = 0; i < arrOutput.length; i++) {
+                                // html상 dom을 바꿔도 doc은 바뀌지 않는다. 다른것으로 생각해야함.
+                                // JSON 형식으로 DB와 주고받는 것으로 수정
+                                // 한 블록안에 여러개의 이미지를 넣는 경우 처리
+                                if (arrOutput[i].content) {
+                                    for (let j = 0; j < arrOutput[i].content.length; j++) {
+                                        let contentInDoc = arrOutput[i].content[j]
+                                        let contentInDocType = contentInDoc['type']
+                                        let contentAttr = contentInDoc['attrs']
+                                        console.log('contentAttr',contentAttr)
+                                        if (contentAttr !== undefined && !contentAttr['title']) {
+                                            contentAttr['title'] = 'try'
+                                            console.log('it is not stored yet!!')
+                                            let imageSrc = contentAttr['src']
+                                            /* 이미지를 s3 서버에 전달 */
+                                            /* 이미지 url 반환받아서 doc JSON 변경 */
+                                            const imageUrl = await imageHandler(imageSrc, userIdx);
+                                            console.log('imagesrc:', imageUrl)
+                                            contentAttr['src'] = imageUrl;
+                                            // console.log('changed val:',imageSrc);
+                                        }
+                                        else {
+                                            if (contentInDocType === 'image')
+                                            console.log('it is alread loaded!!');
+                                        }
+                                        console.log(arrOutput[i], contentInDoc, contentInDocType);
+                                    }
+                                }
                             }
-                            /* solution 1 */
-                            // socket.emit("updateEditor", {id: userID, pdfId: pdfID, text: value});
+
+                            await updateOrClearEditor(userIdx, pdfIdx, jsonOutput);
+                            
                             return value;
                         });
                     })

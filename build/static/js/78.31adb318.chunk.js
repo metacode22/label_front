61b19bae042628e1cc7dba977701d8cc3a378159
NQ-OO <lol1219@snu.ac.kr>
2022-08-05 00:01:00 +0900

    [chore] commmit to merge

---
+++ build/static/js/78.31adb318.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklabel_front=self.webpackChunklabel_front||[]).push([[78],{78:function(e,r,n){n.r(r),n.d(r,{default:function(){return s}});var t="Footer_footer__bY9OR",l=n(6871),a=n(184);function s(){var e=(0,l.TH)().pathname;return(0,a.jsx)(a.Fragment,{children:"/personalreading"!==e?(0,a.jsx)(o,{}):null})}function o(){return(0,a.jsx)("footer",{className:t,children:(0,a.jsx)("div",{children:"\u24d2 Label. All rights reserved."})})}}}]);
+//# sourceMappingURL=78.31adb318.chunk.js.map
